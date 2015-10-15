97b99affda51487f2123adc353962cf499f8c98c
Calvin <calvinsug@gmail.com>
2015-10-15 10:01:32 +0700

    first commit

---
+++ about.php
@@ -0,0 +1,62 @@
+<html>
+
+<head>
+<title>About - Fupa-Toy Store</title>
+<link href="../style.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+<div id="main">
+<?php
+	include("header.php");
+?>
+<hr noshade>
+<div id="content">
+	<div align="center">About<br/>
+
+		<a href="#developers"> Developers	| </a> <a href="#copyright"> Copyright </a> | <a href="#howtoplay"> How to Play</a>
+	</div> 
+
+	<div id="developers">
+
+		<img src="img/developer.jpg">
+		<p align="center">
+			<strong>Developer</strong><br/><br/><br/><br/><br/>
+			Calvin Sugianto<br/>
+			a handsome guy who has a quirk style<br/>
+		</p>
+
+	</div>
+	
+	<div id="copyright"> 
+		<img src="img/banner02.jpg">
+		<p align="center">
+			<strong>Copyright</strong><br/><br/><br/><br/>
+			Copyright © BiskuitLari | 2014. All rights reserved.
+
+		</p>
+	</div>
+	
+	<div id="howtoplay">
+		<img src="img/sonic.jpg">
+		<p align="center">
+				<strong>How to Play</strong><br/><br/><br/>
+				Press up to Jump<br/>
+				Press down to Rolling<br/>
+				Get coin for additional 100 scores<br/>
+				Watch out for the Obstacle
+		</p>
+
+	</div>
+</div>
+
+<div class="clear"></div>
+
+
+<hr noshade>
+<div id="footer">
+	Copyright © BiskuitLari | 2014. All rights reserved.
+</div>
+</div>
+</body>
+</html>
